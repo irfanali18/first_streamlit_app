9655a72bcb3c63a5a936bb5f8a969eb6ec7e7412
Irfan ali <111848490+irfanali18@users.noreply.github.com>
2022-08-23 12:52:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('my parents new healthy diner')
+
+streamlit.header('breakfast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale,spinach & rocket smoothie')
+streamlit.text('hard-boiled free-range egg;)
