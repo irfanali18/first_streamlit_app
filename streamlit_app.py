2fb07c54f7efe77cc1155578a2452340fc4a89fc
Irfan ali <111848490+irfanali18@users.noreply.github.com>
2022-09-05 11:44:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('🐔hard-boiled free-range egg')
 streamlit.text('🥑🍞 avacado toast')
 
 streamlit.header('🍌🥭 build your own fruit smoothie🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
